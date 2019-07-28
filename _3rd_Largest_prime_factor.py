2af699b4aa8a885d6a1d9f46babedb0bd282c235
GrassKaka <rpzheng220807@163.com>
2019-07-28 16:49:57 +0800

    Complete 3rd problem

--- _3rd_Largest_prime_factor.py
+++ _3rd_Largest_prime_factor.py
@@ -15,21 +15,15 @@ What is the largest prime factor of the number 600851475143 ?
 
 # 创建获取被除数最大素因数（除了本身）的函数
 def maxFactor(divided_number):
-    basic_factor = 2  # 基础因数
+    start_factor = 2  # 开始测试的除数
     lastFactor = 1  #
-    while divided_number > 1:
-        if divided_number % basic_factor == 0:  # 判断被除数是否能除尽最新产生的因数
-            lastFactor = basic_factor
-            logging.debug(lastFactor)
-            divided_number = divided_number / basic_factor
-            logging.debug(divided_number)
-            while divided_number % basic_factor == 0:
-                divided_number = divided_number / basic_factor
-                logging.debug(divided_number)
-        basic_factor = basic_factor + 1
-        logging.debug(basic_factor)
+    while divided_number > 1: # 如果被除数大于1，则说明还没找到最大素因数
+        if divided_number % start_factor == 0:  # 判断被除数是否能除尽测试的除数
+            lastFactor = start_factor # 能够除尽，更新最大的素因数
+            divided_number = divided_number / start_factor
+            while divided_number % start_factor == 0:
+                divided_number = divided_number / start_factor
+        start_factor = start_factor + 1 # 将一个测试除数完全除尽，被测试除数加1继续除
     return lastFactor
 
-# logging.debug(maxFactor(600851475143)) # 开始计算
-
-logging.debug(maxFactor(23456)) # 开始计算
+logging.debug(maxFactor(600851475143)) # 开始计算

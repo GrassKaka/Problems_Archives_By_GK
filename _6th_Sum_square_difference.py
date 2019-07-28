4d07f6eaeb026e30c393ef6a238b24afd7932e49
GrassKaka <rpzheng220807@163.com>
2019-07-28 20:46:23 +0800

    Complete 6th problem

---
+++ _6th_Sum_square_difference.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - [%(levelname)s] : %(message)s')
+
+"""
+The sum of the squares of the first ten natural numbers is,
+
+1^2 + 2^2 + ... + 10^2 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers
+and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural 
+numbers and the square of the sum.
+"""
+
+def squreNumber(x):
+    return x * x
+
+logging.debug(sum(range(101))**2 - sum(map(squreNumber, range(101))))

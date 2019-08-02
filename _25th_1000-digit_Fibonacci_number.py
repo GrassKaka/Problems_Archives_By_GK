317daaee1cffc797ba0da8107749cdd14772f25b
GrassKaka <rpzheng220807@163.com>
2019-08-02 15:15:30 +0800

    Complete 25th problem

---
+++ _25th_1000-digit_Fibonacci_number.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - [%(levelname)s] : %(message)s')
+
+"""
+1000-digit Fibonacci number
+
+Problem 25
+The Fibonacci sequence is defined by the recurrence relation:
+
+Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+Hence the first 12 terms will be:
+
+F1 = 1
+F2 = 1
+F3 = 2
+F4 = 3
+F5 = 5
+F6 = 8
+F7 = 13
+F8 = 21
+F9 = 34
+F10 = 55
+F11 = 89
+F12 = 144
+The 12th term, F12, is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+"""
+
+def fibFunc(n):
+    fib1, fib2 = 1, 1
+    for i in range(n-2):
+        fib1, fib2 = fib2, fib1+fib2
+    return fib2
+
+
+n = 1
+while len(str(fibFunc(n))) < 1000:
+    n += 1
+
+logging.debug(n)
